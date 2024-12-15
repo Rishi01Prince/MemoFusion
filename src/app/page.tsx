b3e0618ce100a43fd8ffdc1dd521a9f32108f0f2
Rishi Raj <rishi01prince@gmail.com>
2024-12-15 12:26:00 +0530

    Setup for Super Notes

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { ModeToggle } from "@/components/theme-toggler";
+
+export default function Home() {
+  return(
+    <>
+
+      <ModeToggle/>|
+    </>
+    )
+  ;
+}
