932fbf608cb022006aed35d93a63bc665fb5d661
Rishi Raj <rishi01prince@gmail.com>
2024-12-22 18:10:36 +0530

    Adding sking to the UI

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,11 +1,19 @@
-import Image from "next/image";
+import { Metadata } from "next";
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+
+export const metadata: Metadata = {
+  title:
+    "Next.js E-commerce Dashboard | TailAdmin - Next.js Dashboard Template",
+  description: "This is Next.js Home for TailAdmin Dashboard Template",
+};
 
 export default function Home() {
-  return(
+  return (
     <>
-
-     
+      <DefaultLayout>
+        {/* <ECommerce /> */}
+        <div>hello</div>
+      </DefaultLayout>
     </>
-    )
-  ;
+  );
 }
